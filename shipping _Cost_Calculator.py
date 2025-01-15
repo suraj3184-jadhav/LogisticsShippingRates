abc52bae39c8eaa60f1cf3ae521e1907a22bf660
suraj3184-jadhav <workcse3184@gmail.com>
2025-01-16 02:03:49 +0530

    Update shipping _Cost_Calculator.py

--- shipping _Cost_Calculator.py
+++ shipping _Cost_Calculator.py
@@ -7,5 +7,5 @@ shipping_cost = weight * rate
 ## Display the result
 print(f"Shipping Cost: {shipping_cost} USD")
  # Here is a new update by suraj3184-jadhav
-
+ # Here is a new update by suraj3184-jadhav
 
